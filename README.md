26446e2e8b03983d14b47122f4fa6344240e8e6d
Chuck Danner <chuck.danner@gmail.com>
2021-11-01 20:58:49 -0400

    First Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Hue Command Line Interface #
+This is a project to interact with Phillips Hue lights with a command line interface. 
---
+++ main.go
@@ -0,0 +1,10 @@
+/*
+Copyright © 2021 Chuck Danner chuck.danner@gmail.com
+*/
+package main
+
+import "github.com/chuck-danner/hue/cmd"
+
+func main() {
+	cmd.Execute()
+}
